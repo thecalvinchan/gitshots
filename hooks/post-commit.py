6a3741fdc75456e7960e0fb70625e5481545fb8a
Calvin <calvin.chan.h@gmail.com>
2014-11-18 18:06:40 -0800

    Removed forking

--- hooks/post-commit.py
+++ hooks/post-commit.py
@@ -181,11 +181,11 @@ if __name__ == '__main__':
             print("Upload failed, saving {0}".format(gitshot['sha1']))
 
     # try to fork as soon as possible to not block shell
-    try:
-        if os.fork():  # will not work on windows
-            sys.exit()
-    except AttributeError:
-        pass
+    #try:
+    #    if os.fork():  # will not work on windows
+    #        sys.exit()
+    #except AttributeError:
+    #    pass
 
     gitshot = collect_stats()  # collect the stats
     if GITSHOTS_SERVER_URL:  # upload them
